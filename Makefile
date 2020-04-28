3fb543241c7fc5aa74d97093c8e719f973c14682
Dennis Ruiz <darvein@gmail.com>
2020-04-28 12:32:45 -0400

    adding nginx

--- Makefile
+++ Makefile
@@ -2,7 +2,8 @@ include .env
 
 # Set env vars for terraform
 export TF_VAR_bucket_name=${VERDACCIO_S3_BUCKET}
-export TF_VAR_ssh_key_name=${TF_SSH_KEY_NAME}
+export TF_VAR_ssh_private_key=${TF_SSH_PRIVATE_KEY}
+export TF_VAR_ssh_public_key=${TF_SSH_PUBLIC_KEY}
 export TF_VAR_ec2_subnet_id=${TF_EC2_SUBNET_ID}
 export TF_VAR_ec2_sg_id=${TF_EC2_SG_ID}
 
--- infra/bootstrap.sh
+++ infra/bootstrap.sh
@@ -4,7 +4,7 @@ SVC_USER=ec2-user
 DOCKER_COMPOSE_VERSION=1.25.5
 TARGET_DIR=/opt/verdaccio-s3-master
 
-sudo yum update -y && sudo yum install -y wget unzip
+sudo yum update -y && sudo yum install -y wget unzip nginx
 
 sudo mkdir $TARGET_DIR &&
   sudo chown -R $SVC_USER:$SVC_USER $TARGET_DIR
@@ -21,6 +21,25 @@ wget https://github.com/darvein/verdaccio-s3/archive/master.zip \
 # Setup and run verdaccio
 cp ~/dotenv $TARGET_DIR/.env
 cd $TARGET_DIR && make docker-run
+
+# Setup nginx frontend server
+sudo systemctl enable nginx
+sudo cat << EOF > /etc/nginx/sites-available/verdaccio
+server {
+  listen 80 default_server;
+	access_log /var/log/nginx/verdaccio.log;
+	charset utf-8;
+  location / {
+    proxy_pass              http://localhost:4873/;
+    proxy_set_header        Host $host;
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_set_header X-NginX-Proxy true;
+    proxy_ssl_session_reuse off;
+    proxy_set_header Host $http_host;
+    proxy_redirect off;
+  }
+}
+EOF
 
 # Clean up
 rm ~/dotenv
