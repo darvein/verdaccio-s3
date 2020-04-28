cfd29cab9c1735a4f86d88e05434469961b5d49f
Dennis Ruiz <darvein@gmail.com>
2020-04-28 12:10:42 -0400

    correcting paths

--- infra/bootstrap.sh
+++ infra/bootstrap.sh
@@ -11,13 +11,15 @@ sudo mkdir $TARGET_DIR && sudo chown -R $SVC_USER:$SVC_USER $TARGET_DIR
 sudo curl -L \
   https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m) \
   -o /usr/local/bin/docker-compose
+
 sudo chmod +x /usr/local/bin/docker-compose
 
-wget https://github.com/darvein/verdaccio-s3/archive/master.zip -O /tmp/master.zip
-unzip /tmp/master.zip -d /opt/
+wget https://github.com/darvein/verdaccio-s3/archive/master.zip \
+  -O /tmp/master.zip &&
+  unzip /tmp/master.zip -d /opt/
+
+cp ~/dotenv $TARGET_DIR/.env
+cd $TARGET_DIR && make docker-run
+
+# Clean up
 rm /tmp/master.zip
-
-
-cp ~/dotenv /opt/verdaccio/.env
-
-cd /opt/verdaccio; make docker-run
